a55cf2b438ac0af4eb9cf727bc2be60220514b8c
Gabriel Barbosa <gabriel.a07b@gmail.com>
2021-12-15 13:59:19 -0300

    Removing dependency version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description='Optimization functions',
   packages=['optfuncs'],
   install_requires=[
-    'tensorflow>=2.7.0',
-    'numpy>=1.21.0',
+    'tensorflow',
+    'numpy',
   ]
 )
